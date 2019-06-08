3ecb6db94af5f59247ceaa0468dfeef85d0fe485
dubemelchi <dubemelchi@gmail.com>
2019-06-08 05:41:34 -0400

    added true false buttons

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,19 +1,28 @@
 
+
+//timer variable
 var timeleft = 10;
+
+//timer function
 var gameTimer = setInterval(function(){
+
+   
 
   document.getElementById("timer").innerHTML = timeleft;
 
-  timeleft -= 1;
+  timeleft -= 1; 
 
   if(timeleft <= -2){
+
     clearInterval(gameTimer);
 
-    document.getElementById("timer").innerHTML = "Pencils Down!"
-    
-    window.open("results.html", "replace")
+    document.getElementById("timer").innerHTML = "Pencils Down!";
+
+    window.open("results.html", "_self");
 
   }
 
 }, 1000);
+
+
 
