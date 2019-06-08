f676e9d94a2cb368c09212ae920d001d9ec29fe4
dubemelchi <dubemelchi@gmail.com>
2019-06-08 02:55:28 -0400

    updated js for timer to -2 seconds.

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,15 +1,16 @@
 
-var timeleft = 25;
+var timeleft = 10;
 var gameTimer = setInterval(function(){
 
   document.getElementById("timer").innerHTML = timeleft;
 
   timeleft -= 1;
 
-  if(timeleft <= 0){
+  if(timeleft <= -2){
     clearInterval(gameTimer);
-    document.getElementById("timer").innerHTML = "Finished"
+    document.getElementById("timer").innerHTML = "Pencils Down!"
+    
   }
-  
+
 }, 1000);
 
